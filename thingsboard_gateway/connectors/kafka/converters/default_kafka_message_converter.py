64ca13ada188a8633f7e91c377bcf960b9f97651
Damiano Serpetta <72809849+damianoserpetta@users.noreply.github.com>
2023-12-17 23:25:44 +0100

    Refct: moved on single class for dependency conflicts

--- thingsboard_gateway/connectors/kafka/converters/default_kafka_message_converter.py
+++
@@ -1,9 +0,0 @@
-'''Default converter for Kafka messages'''
-
-
-class DefaultKafkaMessageConverter:
-    '''Default converter for Kafka messages'''
-
-    def convert(self, message) -> dict:
-        '''Converts message to thingsboard data'''
-        return message
--- thingsboard_gateway/connectors/kafka/kafka_connector.py
+++ thingsboard_gateway/connectors/kafka/kafka_connector.py
@@ -4,9 +4,6 @@ from thingsboard_gateway.tb_utility.tb_utility import TBUtility
 from thingsboard_gateway.connectors.connector import Connector
 from thingsboard_gateway.tb_utility.tb_logger import init_logger
 
-from thingsboard_gateway.connectors.kafka.kafka_topic_devices_mapper import KafkaTopicDevicesMapper
-from thingsboard_gateway.connectors.kafka.converters.default_kafka_message_converter import DefaultKafkaMessageConverter
-
 try:
     from kafka import KafkaConsumer
 except ImportError:
@@ -95,3 +92,57 @@ class KafkaConnector(Connector, Thread):
                 self.__gateway.send_to_storage(self.get_name(), dict_result)
                 self.__log.debug("Message sent from topic to device: %s", device["name"])
             self.statistics['MessagesReceived'] += 1
+
+
+class DefaultKafkaMessageConverter:
+    '''Default converter for Kafka messages'''
+
+    def convert(self, message) -> dict:
+        '''Converts message to thingsboard data'''
+        return message
+
+
+class KafkaTopicDevicesMapper:
+    '''Mapper of topics to devices'''
+    def __init__(self, config):
+        self.__config = config
+        self.__mappings = {}
+        self.__load_devices()
+
+    def __load_devices(self):
+        for device in self.__config:
+            if device.get('name') is not None and device.get('topic') is not None and device.get('profile') is not None:
+                self.add_device(device.get('name'), device.get('profile'), device.get('topic'))
+
+    def get_devices_by_topic(self, topic) -> list:
+        '''Returns list of devices in topic'''
+        if topic in self.__mappings:
+            return self.__mappings[topic]
+        else:
+            return None
+        
+    def get_topic(self, device_name) -> str:
+        '''Returns topic for device'''
+        for topic in self.__mappings.items():
+            for device in self.__mappings[topic]:
+                if device.get('device_name') == device_name:
+                    return topic
+        return None
+    
+    def get_topics(self) -> list:
+        '''Returns list of all topics'''
+        return self.__mappings.keys()
+
+    def add_device(self, device_name, device_profile, topic):
+        '''Add device to topic'''
+        self.__create_topic(topic)
+        self.__mappings[topic].append({"name": device_name, "type": device_profile})
+
+    def get_devices(self) -> list:
+        '''Returns list of devices in all topics'''
+        return self.__mappings
+
+    def __create_topic(self, topic):
+        '''Create topic if it doesn't exist'''
+        if topic not in self.__mappings:
+            self.__mappings[topic] = []
